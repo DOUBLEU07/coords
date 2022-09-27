a001370ff13ce19c3d990c2f4f138b9fbd0a5441
DOUBLEU07 <114501385+DOUBLEU07@users.noreply.github.com>
2022-09-28 00:22:19 +0700

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+client_script '@X.Brain/Shared/xGuardPlayer.lua'
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'Hud Coords'
+
+version '1.0'
+
+client_scripts {
+    'coords.lua'
+}
+
+ui_page 'html/index.html'
+
+files {
+    'html/index.html',
+    'html/index.js',
+    'html/style.css'
+}
